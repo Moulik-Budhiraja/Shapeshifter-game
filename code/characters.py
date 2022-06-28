fc564cdf6331cfeb129bcafdc76ce8d885d9fe89
Moulik <moulikbudhiraja@gmail.com>
2022-06-28 15:29:56 -0400

    Implemented movement for blob type

---
+++ code/characters.py
@@ -0,0 +1,136 @@
+from lib2to3.pytree import Base
+import pygame
+from constants import *
+
+
+class BaseCharacter:
+    def __init__(self, pos: tuple, size: tuple, max_health=100):
+        self.x, self.y = pos
+        self.width, self.height = size
+        self.max_health = max_health
+        self.health = max_health
+
+        self.image = Fonts.CHARACTER.render("BASECHR", True, Colors.WHITE)
+        self.image = pygame.transform.scale(
+            self.image, (self.width, self.height))
+
+        self.x_vel = 0
+        self.y_vel = 0
+
+        self.x_accel = 0
+        self.y_accel = 0
+
+    def handle_movement(self, keys):
+        pass
+
+    def transform(self, type: CharacterType):
+        if type == CharacterType.BLOB:
+            new = Blob((self.x, self.y), (self.width,
+                       self.height), self.max_health)
+        elif type == CharacterType.AIRPLANE:
+            new = Airplane((self.x, self.y), (self.width,
+                           self.height), self.max_health)
+        elif type == CharacterType.SPRING:
+            new = Spring((self.x, self.y), (self.width,
+                         self.height), self.max_health)
+        elif type == CharacterType.WEIGHT:
+            new = Weight((self.x, self.y), (self.width,
+                         self.height), self.max_health)
+        elif type == CharacterType.PLUNGER:
+            new = Plunger((self.x, self.y), (self.width,
+                          self.height), self.max_health)
+
+        new.health = self.health
+
+        new.x_vel = self.x_vel
+        new.y_vel = self.y_vel
+
+        new.x_accel = self.x_accel
+        new.y_accel = self.y_accel
+
+        return new
+
+    def draw(self, win):
+        win.blit(self.image, (self.x, self.y))
+
+
+class Blob(BaseCharacter):
+    def __init__(self, pos: tuple, size: tuple, max_health=100):
+        super().__init__(pos, size, max_health)
+        self.image = Fonts.CHARACTER.render("BLOB", True, Colors.WHITE)
+        self.image = pygame.transform.scale(
+            self.image, (self.width, self.height))
+
+        self.jumping = False
+        self.jump_vel = 8
+
+    def handle_movement(self, keys):
+        if keys[pygame.K_LEFT] == keys[pygame.K_RIGHT]:
+            self.x_accel = 0
+        elif keys[pygame.K_LEFT]:
+            self.x_accel = -1
+        elif keys[pygame.K_RIGHT]:
+            self.x_accel = 1
+
+        if keys[pygame.K_UP]:
+            if not self.jumping:
+                self.jumping = True
+                self.y_vel = -self.jump_vel
+
+        # If not colliding with terrain
+
+        if self.jumping:
+            self.y_accel = 0.2
+        else:
+            self.y_accel = 0
+
+        self.y_vel += self.y_accel
+        self.x_vel += self.x_accel
+
+        if self.x_vel > 3:
+            self.x_vel = 3
+        elif self.x_vel < -3:
+            self.x_vel = -3
+
+        self.y_vel = round(self.y_vel, 1)
+
+        if self.y_vel == self.jump_vel:
+            self.jumping = False
+            self.y_vel = 0
+
+        self.x += self.x_vel
+        self.y += self.y_vel
+
+        print(self.y_vel, self.y)
+
+
+class Airplane(BaseCharacter):
+    def __init__(self, pos: tuple, size: tuple, max_health=100):
+        super().__init__(pos, size, max_health)
+        self.image = Fonts.CHARACTER.render("AIRPLANE", True, Colors.WHITE)
+        self.image = pygame.transform.scale(
+            self.image, (self.width, self.height))
+
+
+class Spring(BaseCharacter):
+    def __init__(self, pos: tuple, size: tuple, max_health=100):
+        super().__init__(pos, size, max_health)
+        self.image = Fonts.CHARACTER.render("SPRING", True, Colors.WHITE)
+        self.image = pygame.transform.scale(
+            self.image, (self.width, self.height))
+
+
+class Weight(BaseCharacter):
+    def __init__(self, pos: tuple, size: tuple, max_health=100):
+        super().__init__(pos, size, max_health)
+        self.image = Fonts.CHARACTER.render("WEIGHT", True, Colors.WHITE)
+        self.image = pygame.transform.scale(
+            self.image, (self.width, self.height))
+
+
+class Plunger(BaseCharacter):
+    def __init__(self, pos: tuple, size: tuple, max_health=100):
+        super().__init__(pos, size, max_health)
+        self.image = Fonts.CHARACTER.render("PLUNGER", True, Colors.WHITE)
+        self.image = pygame.transform.scale(
+            self.image, (self.width, self.height))
--- code/main.py
+++ code/main.py
@@ -1,10 +1,7 @@
 import pygame
+from constants import *
+from characters import Blob, Airplane, Spring, Weight, Plunger
 pygame.init()
-
-WIDTH = 1120
-HEIGHT = 630
-
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
 class Game:
@@ -12,16 +9,20 @@ class Game:
         self.width = width
         self.height = height
 
+        self.WIN = pygame.display.set_mode((width, height))
+
     def run(self):
         self.setup()
         self.loop()
 
     def setup(self):
+        self.character = Blob((WIDTH / 2, HEIGHT / 2), (60, 60))
 
+        self.clock = pygame.time.Clock()
         self.running = True
 
     def loop(self):
-        while self.running():
+        while self.running:
             self.update()
             self.draw()
 
@@ -31,6 +32,38 @@ class Game:
                 pygame.quit()
                 quit()
 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    self.character = self.character.transform(
+                        CharacterType.BLOB)
+                elif event.key == pygame.K_2:
+                    self.character = self.character.transform(
+                        CharacterType.AIRPLANE)
+                elif event.key == pygame.K_3:
+                    self.character = self.character.transform(
+                        CharacterType.SPRING)
+                elif event.key == pygame.K_4:
+                    self.character = self.character.transform(
+                        CharacterType.WEIGHT)
+                elif event.key == pygame.K_5:
+                    self.character = self.character.transform(
+                        CharacterType.PLUNGER)
+
+        self.character.handle_movement(pygame.key.get_pressed())
+
     def draw(self):
-        WIN.fill((0, 0, 0))
+        self.WIN.fill(Colors.BLACK)
+
+        self.character.draw(self.WIN)
+
         pygame.display.update()
+
+        self.clock.tick(60)
+
+
+if __name__ == '__main__':
+    WIDTH = 1120
+    HEIGHT = 630
+
+    game = Game(WIDTH, HEIGHT)
+    game.run()
